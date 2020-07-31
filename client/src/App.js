8468d316eaef70c8b031ec69ec7aa78b947d9299
shivani jha <shivani261979@gmail.com>
2020-07-31 18:09:56 -0400

    cards componenets

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Nav from "./components/Navbar";
+import Card from "./components/Card";
 // import your component here
 
 function App() {
@@ -14,6 +15,32 @@ function App() {
         <Home />        
         </Route>
       </Switch>
+    </div>
+    <div>
+      <div class="MyCard">
+        <Card />
+        <Switch>
+          <Route exact path={["/", "/home"]}>
+          <Home />        
+          </Route>
+        </Switch>
+      </div>
+      <div class="MyCard">
+        <Card />
+        <Switch>
+          <Route exact path={["/", "/home"]}>
+          <Home />        
+          </Route>
+        </Switch>
+      </div>
+      <div class="MyCard">
+        <Card />
+        <Switch>
+          <Route exact path={["/", "/home"]}>
+          <Home />        
+          </Route>
+        </Switch>
+      </div>
     </div>
   </Router>
    );
