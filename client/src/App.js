7e4a9aa32593b5a4ad5d2ca6cccf724307ce09d8
deej17 <61299175+deej17@users.noreply.github.com>
2020-08-01 10:14:41 -0400

    models and routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,27 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Nav from "./components/Navbar";
+//import Carousel from "./components/Carousel";
+import BootstrapCarousel from './components/Carousel' 
+
 // import your component here
 
 function App() {
   return (
-  <Router>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path={["/", "/home"]}>
-        <Home />        
-        </Route>
-      </Switch>
-    </div>
-  </Router>
-   );
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path={["/", "/home"]}>
+          <BootstrapCarousel></BootstrapCarousel>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 
 export default App;
+
