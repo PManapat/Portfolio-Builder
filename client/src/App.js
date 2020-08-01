22d171cc187e76d8467579ae28714f991d61ca89
surajgurung <microbiosuraj@gmail.com>
2020-08-01 13:46:16 -0400

    change on app.js and home.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,55 +4,28 @@ import Home from "./pages/Home";
 import Nav from "./components/Navbar";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
-import Carousel from "./components/Carousel"; 
+// import Carousel from "./components/Carousel"; 
 import Footer from "./components/Footer";
-import Card from "./components/Card";
-import Jumbotron from "./components/Jumbotron";
+// import Card from "./components/Card";
+// import Jumbotron from "./components/Jumbotron";
 
 function App() {
   return (
   <Router>
     <div>
       <Nav />
-      <Switch>
-        <Jumbotron />
-        <Route exact path={["/", "/home"]}>
+      {/* <Switch> */}
         
-        <Home />        
-        </Route>
+        
+       
         <Route exact path="/register" component={Register}/>
           <Route exact path="/login" component={Login}/>
-      </Switch>
+          <Route exact path={["/", "/home"]} component={Home}/>
+      
+      
+      {/* </Switch> */}
     </div>
-    <div>
-    <Carousel />
-    </div>
-    <div>
-      <div className="MyCard">
-        <Card />
-        <Switch>
-          <Route exact path={["/", "/home"]}>
-          <Home />        
-          </Route>
-        </Switch>
-      </div>
-      <div className="MyCard">
-        <Card />
-        <Switch>
-          <Route exact path={["/", "/home"]}>
-          <Home />        
-          </Route>
-        </Switch>
-      </div>
-      <div className="MyCard">
-        <Card />
-        <Switch>
-          <Route exact path={["/", "/home"]}>
-          <Home />        
-          </Route>
-        </Switch>
-      </div>
-    </div>
+    
     <Footer />
   </Router>
    );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,45 @@
 import React from "react";
 import Jumbotron from "../components/Jumbotron";
+import Carousel from "../components/Carousel"; 
+
+import Card from "../components/Card";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function Home (){
 return(
-    <div>This is from Home.js</div>
+    <div>
+        <div>
+        <Jumbotron />
+    <Carousel />
+    </div>
+    <div>
+      <div className="MyCard">
+        <Card />
+        <Switch>
+          <Route exact path={["/", "/home"]}>
+          <Home />        
+          </Route>
+        </Switch>
+      </div>
+      <div className="MyCard">
+        <Card />
+        <Switch>
+          <Route exact path={["/", "/home"]}>
+          <Home />        
+          </Route>
+        </Switch>
+      </div>
+      <div className="MyCard">
+        <Card />
+        <Switch>
+          <Route exact path={["/", "/home"]}>
+          <Home />        
+          </Route>
+        </Switch>
+      </div>
+    </div>
+    </div>
 )
 }
 
