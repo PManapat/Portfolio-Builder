56f47fa890a6c9ffd1b10c4c922746fbe08489c5
Rich Capuy <rccapuy@gmail.com>
2020-08-10 20:30:04 -0400

    paths to unique url

--- client/src/pages/MainProfile.js
+++ client/src/pages/MainProfile.js
@@ -2,33 +2,40 @@ import React, { useEffect, useState } from 'react';
 import "./MainProfile.css";
 import { home } from "../utils/api.js";
 import Dashboard from '../components/Dashboard';
-// import { mainProfile } from "../utils/api.js";
+// import { useHistory } from "react-router-dom";
+import { Button } from 'react-bootstrap';
+
 
 export default () => {
+    // let history = useHistory();
     const[firstName, setFirstName]=useState("");
-    // const[lastName, setLastName]=useState("");
-    // const[email, setEmail]=useState("");
-    
+    const[lastName, setLastName]=useState("");
+    // const[portfolio, setPortfolio]=useState("/template");
+
     useEffect(() => {
         home()
         .then(res => {
             console.log(res);
-            // const{ user:{firstName, lastName, email}}=res;
-            const{firstName}=res;
+            const{firstName, lastName}=res;
             console.log({firstName});
             setFirstName({firstName});
-            // setLastName({lastName});
-            // setEmail({email});
-        })
-        .catch(err => console.log(err));
+            setLastName({lastName});
+            // setPortfolio(`/${firstName}`);
+        }).catch(err => console.log(err));
     },[])
+
+    // function handleClick(){
+    //     console.log(`${firstName}${lastName}`)
+    //     return (history.push(`${firstName}${lastName}`))
+    // }
 
     return (
         <div className="container" id="container">
             <div className="jumbotron mt-5">
                 <div className="col-sm-8 mx-auto">
-                <h2 className="text-center">Hello {firstName.firstName}</h2>
+    <h2 className="text-center">Hello {firstName.firstName} {" "}{lastName.lastName}</h2>
                     <h5>This page is limited. We will be updating features.</h5>
+                    {/* <Button onClick={handleClick} name="Check your Portfolio!">Check portfolio</Button> */}
                 </div>
             </div>
             <Dashboard />
