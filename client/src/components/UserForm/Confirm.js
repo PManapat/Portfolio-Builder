431db4610fc8fd6fa4ad7465d14e63787bd429f5
Rich Capuy <rccapuy@gmail.com>
2020-08-12 15:10:14 -0400

    button customized at end of customize form

--- client/src/components/UserForm/Confirm.js
+++ client/src/components/UserForm/Confirm.js
@@ -14,6 +14,7 @@ import TypoGraphy from "@material-ui/core/Typography";
 import Toolbar from "@material-ui/core/Toolbar";
 // import Nav from "../UserNav";
 import { grey } from "@material-ui/core/colors";
+import { withStyles } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
   palette: {
@@ -141,6 +142,23 @@ export class Confirm extends Component {
         footer,
       },
     } = this.props;
+
+    const StyledButton = withStyles({
+      root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 36,
+        size: 'large',
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      },
+      label: {
+        textTransform: 'capitalize',
+      },
+    })(Button);
+
     return (
       <MuiThemeProvider theme={theme}>
         <>
@@ -272,13 +290,7 @@ export class Confirm extends Component {
                 </Button>
               </div>
               <div style={{marginLeft: '10px', marginRight: '10px'}}>
-                <Button
-                  color="primary"
-                  variant="contained"
-                  onClick={this.continue}
-                >
-                  Continue
-                </Button>
+              <StyledButton variant="contained" onClick={this.continue}>CONFIRM</StyledButton>
               </div>
             </div>
           <br />
